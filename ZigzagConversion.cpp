bebc4475eb6686df7ef24d85e531c6ab923293f6
Arsi <syedarsalaan12@gmail.com>
2022-06-26 23:06:53 +0530

    daily commit

--- ZigzagConversion.cpp
+++ ZigzagConversion.cpp
@@ -3,6 +3,9 @@ using namespace std;
 class Solution {
 public:
     string convert(string s, int numRows) {
+        if(numRows == 1){
+            return s;
+        }
         int n = s.length();
         vector<string> vec;
         for(int i = 0; (i < numRows) && (i < n); i++){
@@ -10,30 +13,28 @@ public:
             vec.push_back(stemp);
         }
         
-        int j = vec.size();
+        int j = vec.size() - 1;
         bool flag = false;
-        s = s.substr(j);
-        int i = 0;
-        for(auto x : s){
-            string stemp(1, x);
-            vec[j].append(stemp);
+        s = s.substr(j + 1);
 
-            if(i%vec.size() == 0){
+        for(int i = 0; i < s.length(); i++){
+           
+            if(j == vec.size() -1 || j ==0){
                 flag = !flag;
             }
-
             if(flag){
                 j--;
             }
             else{
                 j++;
             }
-            i++;
+            string stemp(1, s[i]);
+            vec[j].append(stemp);
         }
         
         string result = "";
         for(auto x : vec){
-            //result = result + x;
+            result = result + x;
             cout << x << '\n';
         }
 
